36604272b28d24fc6b7b45e2b4bba992b08d4dbb
Wladek <wladek.santos@spassu.com>
2021-03-23 14:55:46 -0300

    Add models

--- short_url/models.py
+++ short_url/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from uuid import uuid4
+from django.utils.timezone import now
+from datetime import timedelta
 
-# Create your models here.
+
+class UrlShortened(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    url = models.URLField(max_length=255, null=True)
+    _name = models.CharField(max_length=255, null=True,
+                             unique=True, name='name')
+    date_expiration = models.DateTimeField(default=now() + timedelta(7))
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not value:
+            self._name = str(uuid4())[:5]
+        else:
+            self._name = value
+
+    def __str__(self):
+        return self.url
